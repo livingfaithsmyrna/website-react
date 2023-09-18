3ebd401dd03ed252eaccedf18616d5d78ec2560e
livingfaithsmyrna <livingfaith208@gmail.com>
2023-09-18 10:10:12 -0500

    New picture and new button in Hero section on Home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -11,14 +11,7 @@ function Home() {
             <div className="app__home-hero-content">
               <div className="app__home-hero-title">Come grow with us!</div>
               <span>208 Jefferson Pk. | Sundays @ 11 am</span>
-              {/*<span>We are a loving church excited about growing in the Lord.</span>*/}
-              {/*<span>Help us make a difference in our community!</span>*/}
-              {/*<button className="app__home-button"><img src={podcastIcon}/><span>Live Stream</span></button>*/}
-              <Button
-                  type="submit"
-                  text="Live Stream"
-                  icon="fa-podcast"
-              />
+              <Button type="submit" text="Live Stream" icon="podcast" />
             </div>
           </div>
         </div>
