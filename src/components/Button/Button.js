2b30ea33594d2e77a8ca9e711d674e597146723d
livingfaithsmyrna <livingfaith208@gmail.com>
2023-09-18 12:40:13 -0500

    Button is dynamic and looks good. Removed Bable Macros.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -7,16 +7,18 @@ npm i --save @fortawesome/free-regular-svg-icons
 npm i --save @fortawesome/free-brands-svg-icons
 npm i --save @fortawesome/react-fontawesome@latest
 
+Simplest way to do it
+https://dev.to/sobhandash/how-to-simply-use-font-awesome-6-in-react-1bm8
 */
 
 import React from 'react';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPodcast } from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {icon} from '@fortawesome/fontawesome-svg-core/import.macro'
 import './Button.css'
 
 const Button = (props) => {
-  const buttonIcon = icon({name: 'podcast'})
-
+  library.add(faPodcast)
   return (
       <button
         type={props.type || "button"}
@@ -26,7 +28,7 @@ const Button = (props) => {
       >
         <span className={"button__text"}>{props.text}</span>
         <span className={"button__icon"}>
-          <FontAwesomeIcon icon={buttonIcon} />
+          <FontAwesomeIcon icon={props.icon} />
         </span>
       </button>
   );
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Button from "../../components/Button/Button";
 import './Home.css'
-import podcastIcon from '../../assets/podcasts_FILL0_wght400_GRAD0_opsz24.png'
 
 function Home() {
   return (
